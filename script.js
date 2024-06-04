b7f6dfc5298cb1bf36fa9b6386e47b759bf8ad1c
Cvetanka <apostolovacvetanka4@gmail.com>
2024-06-04 09:51:29 +0200

    changes in js

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const buttons = document.querySelectorAll(".card-item-btn");
 buttons.forEach((button) => {
  button.addEventListener("click", function () {
   this.classList.toggle("active");
-  const descriptions = this.nextElementSibling;
+  const descriptions = this.nextElementSibling; //the next sibling element of the clicked button
   const plusIcons = this.querySelector(".plus-icon");
   const minusIcons = this.querySelector(".minus-icon");
 
   if (descriptions.style.maxHeight) {
    descriptions.style.maxHeight = null;
-   plusIcons.style.display = "block";
-   minusIcons.style.display = "none";
+   plusIcons.style.display = "block"; // making it visible.
+   minusIcons.style.display = "none"; //hiding it.
   } else {
    descriptions.style.maxHeight = descriptions.scrollHeight + "px";
    plusIcons.style.display = "none";
